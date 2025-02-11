adf587f1b97b555d8b70ac181bc914399f276766
Basang Basangov <m300609@mail.ru>
2025-02-11 15:10:08 +0300

    fix: home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,33 @@
 import { PromoSlider } from '@/shared/ui';
-import { PromoSlides } from './content';
+import { PromoSlides } from './content.js';
 
-export const Home = () => <PromoSlider content={PromoSlides} />;
+import { Cards } from '@/components/Cards/Cards';
+import {
+  useGetTopBoxOfficeMoviesQuery,
+  useGetIndiaUpcomingMoviesQuery,
+} from '@/features/movies/moviesApiSlice';
+
+export function Home() {
+  const { data: topBoxOfficeMovies } = useGetTopBoxOfficeMoviesQuery({});
+  const { data: indiaUpcomingMovies } = useGetIndiaUpcomingMoviesQuery({});
+
+  return (
+    <>
+      <PromoSlider content={PromoSlides} />
+
+      {topBoxOfficeMovies ? (
+        <Cards title='Советуем к просмотру' data={topBoxOfficeMovies} />
+      ) : (
+        <p>Загрузка...</p>
+      )}
+      {indiaUpcomingMovies ? (
+        <Cards
+          title='Самые ожидаемые фильмы в Индии'
+          data={indiaUpcomingMovies}
+        />
+      ) : (
+        <p>Загрузка...</p>
+      )}
+    </>
+  );
+}
